fa79c955678a79e4cae50569db51dadab346751c
Will-Peterson <william.from.ca@gmail.com>
2020-08-26 11:24:12 -0700

    added react router to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// const Footer = () => {
+//     return (
+//         <div className='footer'>
+//             <div>About Us | Contact Us | Privacy Policy | Terms of Service</div>
+//             <div>&copy; {new Date().getFullYear()} asciicode.ws</div>
+//             <div>PetersonDigital</div>
+//         </div>
+//     );
+// }
+
 
 const Footer = () => {
     return (
-        <div className='Footer'>
-            <div>About Us | Contact Us | Privacy Policy | Terms of Service</div>
-            <div>&copy; {new Date().getFullYear()} asciicode.ws</div>
-            <div>PetersonDigital</div>
+        <div className='footer'>
+            <Link to='/about'>About Us</Link> |&nbsp;
+            <Link to='/contact'>Contact Us</Link> |&nbsp;
+            <Link to='/terms-of-service'>Terms of Service</Link> |&nbsp;
+            <Link to='/privacy-policy'>Privacy Policy</Link>
+            <div>&copy; {new Date().getFullYear()}&nbsp;
+            <Link to='/'>asciicode.ws</Link></div>
+            <div><a href='http://petersondigital.co'>PetersonDigital, LLC</a></div>
         </div>
     );
 }
+
+
 
 export default Footer;
