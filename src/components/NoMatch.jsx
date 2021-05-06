0645dea11840ae87cc9ca2b239668a4b31c63bf6
Will-Peterson <william.from.ca@gmail.com>
2021-05-06 16:00:14 -0700

    feat: added 404 page

--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../App.css';
+import Button from 'react-bootstrap/button';
+import {Link} from 'react-router-dom';
+
+import NoMatchImage from '../assets/images/404bg.jpg';
 
 const NoMatch = () => {
   return (
-      <React.Fragment>
-        <h2>404 ERROR - PAGE DOES NOT EXIST</h2>
-      </React.Fragment>
+      <div className='no-match-container'>
+        <img className='no-match-image' src={NoMatchImage} />
+        <div className='oops'>Oops! Page does not exist!</div>
+        <Button variant='dark' className='home-button'><Link to='/'>Home</Link></Button>
+      </div>
   );  
 };
 
