ab968e295129b43faa9c6dc15ab28c1960fa798e
Will-Peterson <william.from.ca@gmail.com>
2021-05-06 22:26:32 -0700

    refactor: removed unused code in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import * as ReactBootStrap from 'react-bootstrap';
-
-
-// import Home from './Home';
-// import Contact from './Contact';
-// import About from './About';
-// import TermsOfService from './TermsOfService';
-// import PrivacyPolicy from './PrivacyPolicy';
-// import NoMatch from './NoMatch';
-
-// import Contact from './Contact';
-// import About from './About';
-// import NoMatch from './NoMatch.js';
-
-// import Navbar from 'react-bootstrap/navbar';
-// import Nav from 'react-bootstrap/nav';
-// import Button from 'react-bootstrap/button';
-// import Form from 'react-bootstrap/form';
-// import FormControl from 'react-bootstrap/formcontrol';
-
-// const Header = () => {
-//   return (
-    
-//       <div>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/about">About</Link>
-//           </li>
-//           <li>
-//             <Link to="/terms-of-service">Terms of Service</Link>
-//           </li>
-//           <li>
-//             <Link to="/privacy-policy">Privacy Policy</Link>
-//           </li>
-//           <li>
-//             <Link to="/contact">Contact</Link>
-//           </li>
-//         </ul>
-//         <hr />
-//       </div>
-    
-//   );
-// }
-
 
 const Header = () => {
   return (
@@ -60,7 +13,6 @@ const Header = () => {
           <ReactBootStrap.Nav.Link href="terms-and-conditions">Terms and Conditions</ReactBootStrap.Nav.Link>
           <ReactBootStrap.Nav.Link href="privacy-policy">Privacy Policy</ReactBootStrap.Nav.Link>
         </ReactBootStrap.Nav>
-        
     </ReactBootStrap.Navbar.Collapse>
   </ReactBootStrap.Navbar>
   );
