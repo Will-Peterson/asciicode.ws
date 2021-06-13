724ae1666176ef64a71934f6d67c826cfedca932
Will-Peterson <william.from.ca@gmail.com>
2021-06-12 17:09:14 -0700

    feat: add text snippet to contact form
    Added text snippet with styles to contact form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,10 @@ const Contact = () => {
     <FormspreeProvider project="1665506654826790283">
       <div className="container-contact">
         <h2>CONTACT</h2>
+        <div style={{fontSize: '1.3vm',margin: '-50px auto 30px',backgroundColor:'rgb(238,228,228',padding:'.8vw',borderLeft:'3px solid blue'}}>If you have any questions, comments, or improvements, please fill 
+          in the contact form and let us know. We read and look forward to all 
+          submissions. Your valued comments help us improve this website for 
+          everyone's benefit.</div>
         <ContactForm />
       </div>
     </FormspreeProvider>
